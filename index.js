34214c87922317639f9e0be3b599ea059cb799f2
Vishnu Ks <hackerkid@vishnuks.com>
2015-10-28 12:46:59 +0530

    renoved comments from the final code

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 
 function get(username, password, callback) {
-  var url = "https://api.github.com/notifications"; // remember var or we create a global variable
+  var url = "https://api.github.com/notifications";
   xhr.open("GET", url, true, username, password);
   xhr.onreadystatechange = function() {
-    var error, responseObj; // moving variable declaration to the top of the function as js variables are always hoisted to here anyway
+    var error, responseObj; 
 
     if (this.readyState === 4) {
 
       try {
         responseObj = JSON.parse(this.responseText);
-      } catch(err) { // The JSON was malformed
+      } catch(err) {
         error = err;
       }
 
-      if(/^[45]\d\d/.test(this.status)) // Test if the server gives us 400 or 500 errors, see https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+      if(/^[45]\d\d/.test(this.status))
         error = new Error(this.status + ": " + this.statusText)
 
       callback(error, responseObj);
     }
   };
-  xhr.send(); // moved to bottom to avoid race condition (onreadystatechange could be called before we are listening)
+  xhr.send(); 
 }
 
 module.exports = {
